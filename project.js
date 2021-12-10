099aeba512713b848a5aebda89633e92b2d4501e
VeredBarArbel <veredarbel@gmail.com>
2021-12-10 13:47:36 +0200

    final pages with nav bar

--- project.js
+++ project.js
@@ -2,19 +2,6 @@ function buttonVisibility(buttonID) {
     document.getElementById(buttonID).style.visibility = "visible";
 }
 
-//profile - show-hide info
-var infoIndex = 0;
-showHide(0);
-
-function showHide(index) {
-    var infoPages = document.getElementsByClassName("info");
-    var size = infoPages.length;
-    if (index < 0) {index = size-1}
-    else if (index >= size) {index = 0}
-    var i;
-    for (i = 0; i < size; i++){
-        infoPages[i].style.display = "none";
-    }
-    infoPages[i].style.display = "show";
-
+function notification(elementId, message) {
+   alert(message);
 }
